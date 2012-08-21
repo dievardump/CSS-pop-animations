356b891658c33bebfb53c73a01b4db489e60c7c1
dievardump <simon.fremaux@gmail.com>
2012-08-21 18:44:39 -0400

    adding menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,29 @@
 (function () {
 	'use strict';
-	var container = document.getElementById('container');
-	if (container) {
-		document.getElementById('toggle').addEventListener('click', function (event) {
-			container.className = '';
-			setTimeout(function () { container.className = 'animate'; }, 300);
-		}, false);
-	}
+	var links = document.getElementsByClassName('links')[0].getElementsByTagName('a'),
+		targets = document.getElementsByClassName('target'),
+		current = 0,
+		className = 'pop';
+
+	function removeClass (element, className) {
+		element.className = element.className.replace(new RegExp('(^|\\s)' + className + '(?:\\s|$)'), '$1');
+	};
+
+	function addClass (element, className) {
+		element.className += ' ' + className;
+	};
+
+	Array.prototype.forEach.call(links, 
+		function (link, i) {
+			link.addEventListener('click', 
+				function (event) {
+					event.preventDefault();
+					removeClass(targets[current], className);
+					current = i;
+					addClass(targets[current], className);
+			}, false);
+		}
+	);
+
+	addClass(targets[current], className);
 }());
